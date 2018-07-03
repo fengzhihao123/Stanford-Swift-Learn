7723eef901d4b31ef2db2cab92d2affb6a6aec62
fengzhihao <1374795493@qq.com>
2018-07-03 20:28:36 +0800

    添加playingCard-0703

---
+++ StanfordDemo/PlayingCard/PlayingCard.swift
@@ -0,0 +1,22 @@
+//
+//  PlayingCard.swift
+//  PlayingCard
+//
+//  Created by 冯志浩 on 2018/7/3.
+//  Copyright © 2018年 冯志浩. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCard: UIView {
+    override func draw(_ rect: CGRect) {
+        
+//        let path = UIBezierPath()
+//        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+//        path.lineWidth = 5
+//        UIColor.green.setFill()
+//        UIColor.red.setStroke()
+//        path.stroke()
+//        path.fill()
+    }
+}
